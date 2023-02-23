16b5ce3c1d859ac7a2b1e8b60a207a16daff5680
Elias Tsolis <41580704+estatistics@users.noreply.github.com>
2023-02-24 00:21:18 +0200

    Update -A4_Reading_Writing_SAV_Files.R

--- -A4_Reading_Writing_SAV_Files.R
+++ -A4_Reading_Writing_SAV_Files.R
@@ -1,10 +1,10 @@
 library(haven)
 
 # Reading sav file
-df_sav <- read_sav("/home/elias/Data/STATISTICS/Niki/data_pspp.sav", user_na=T)
+df_sav <- read_sav("/home/data_pspp.sav", user_na=T)
 
 # Reading csv file
-pth="/home/elias/Data/STATISTICS/Niki/Data_For_Analysis/"
+pth="/home/Data_For_Analysis/"
 file="data_whole.csv"
 
 data_0     = read.csv( paste0(pth, file), header = TRUE, sep = ",", quote = "" )
@@ -12,5 +12,5 @@ data_0     = read.csv( paste0(pth, file), header = TRUE, sep = ",", quote = "" )
 df_sav$Περιφέρεια <- trimws( data_0$Περιφέρεια )
 
 
-write_sav(df_sav, "/home/elias/Data/STATISTICS/Niki/data_pspp_0.sav")
+write_sav(df_sav, "/home/data_pspp_0.sav")
 
