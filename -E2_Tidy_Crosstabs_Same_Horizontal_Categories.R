b056c0cb738cd4d8be23748f4acbaa7522478b81
Elias Tsolis <41580704+estatistics@users.noreply.github.com>
2024-01-07 19:47:13 +0200

    Update -E2_Tidy_Crosstabs_Same_Horizontal_Categories.R

--- -E2_Tidy_Crosstabs_Same_Horizontal_Categories.R
+++ -E2_Tidy_Crosstabs_Same_Horizontal_Categories.R
@@ -14,8 +14,7 @@
 ##########################################################
 
 # Depends on Base R / stats only
-
-
+# To write results may require(RCurl)
 
 ##########################################################
 #### Example DF                                   #####
@@ -23,221 +22,151 @@
 
 # SEE at the end of this file
 
-
 ##########################################################
 ####Arguments of the Function                       #####
 ##########################################################
 
-# path_of_csvs:  The path where the Crosstabulation Tables from PSPP 
-#                are found e.g. "/home/crosstabs_csvs/"
+# path_csv : you must define the whole path eg.  pth_file=/home/USER/data/
+# In this path output results will be written
+#
+# file_name : you must define the whole file name eg. crosstabs.csv
+#
+# csv_opts: default to read.csv( paste0(path_csv, file_name), header = FALSE, sep = ",", quote = "", na.strings="." )
+# 
+# pth to open csv files: eg. 
+# tbl_rows : you must include the row stats that you need: tbl_rows=c('Count','Row','Col', "rTotal") 
+# Row, Column stats are only included if exist in csv and specified here. They refer to "Row %" and "Col %"
 
+# chqstats : you must include the stats that you need: 
+# chqstats=c('Cramersv', 'Phi',  'Contingency', 'taub', 'tauc', 'tauc', 'Gamma', 'Spearman', 'Pearson')
+
+#
 
 ##########################################################
 ####  The Function                              #####
 ##########################################################
 
 
+crosstabs_same_horizontal_labels <- function(path_csv="", file_name="", csv_opts=FALSE,
+                                             tbl_rows=c('Count','Row','Col', "rTotal"), rsumTotal=0, 
+                                             chqstats=c('Cramersv', 'Phi',  'Contingency', 'taub', 'tauc', 'tauc', 'Gamma', 'Spearman', 'Pearson') ) {
 
-crosstabs_same_horizontal_categories <- function( path_of_csvs ) {
+            
+  csv_files = read.csv( paste0(path_csv, file_name), header = FALSE, sep = ",", quote = "", na.strings="." )
+  str(csv_files)
   
-  
+  # Reading csv file
+  if (csv_opts == FALSE)  csv_files=csv_files   else if (csv_opts != FALSE)     csv_files = csv_opts
   
   #### Making an operator
   # Found on stackoverflow "5831794"
   '%!in%' <- Negate(`%in%`)
   
-  
-  # Found on stackoverflow: 11433432
-  # Reading multiple csv files
-  pth        = path_of_csvs
-  dirs_tmp   = paste0(pth, list.files(pth, pattern = "*.csv" ) )
-  csv_files  = lapply(dirs_tmp, function(x) read.csv(x,  header = FALSE, sep = ",", quote = "" ) )
-  
-  # Separating Crosstable tables / Names / Pearsons statistics 
-  
-  rbind_cross <- list()
-  rbind_chsq  <- list()
-  test_nm  <- list()
-  var_names<- list()
-  for ( d in 1:length( csv_files ) ) {
-    
-    crosstbs <- csv_files[[d]]
-    crosstbs$id <- 1:(dim( crosstbs )[1])
-    
-    tbl_st   <- crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], "total") ] + 2, ] 
-    tbl_fn   <- crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], "Total") ], ]
-    test_nm0 <- crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], " \\* ") ], ]
-    test_nm  <- test_nm0[test_nm0$id %!in% test_nm0[grep(x=test_nm0[[1]], "tot"),]$id, ]
-    test_id  <- test_nm0[test_nm0$id %in% test_nm0[grep(x=test_nm0[[1]], "tot"),]$id, ]$id+1
-    cq_ln    <- crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], "-Square") ], ]
-    
-    st_pos  <- 0
-    fn_pos  <- unlist( gregexpr(pattern =" \\*", test_nm[[1]]) )
-    # nchar(as.matrix( test_nm[1,][[1]][1] ))
-    
-    tables_ln <- list()
-    nm_tbls   <- list()
-    chsq_lns  <- list()
-    
-    for( i in 1:dim( tbl_st )[1] ) {
-      crosstbs[tbl_fn$id[i], ] <-""
-      tables_ln[[i]]           <- crosstbs[tbl_st$id[i]:tbl_fn$id[i], ]  
-      nm_tbls[[i]]             <- tables_ln[[i]][1,]
-      chsq_lns[[i]]            <- crosstbs[cq_ln$id[i], ]  
-      
-    }
-    
-    ### ==========================================###
-    ###   The rbind_null_df_lists function  ###
-    ### ========================================= ###
-    
-    # The function to rbind it
-    rbind_null_df_lists <- function ( list_of_dfs ) {
-      
-      # check code
-      # tables_ln
-      # list_of_dfs <-   list_df 
-      
-      
-      if ( length(names(list_of_dfs)) == 0 ) {
-        
-        names(list_of_dfs) <- 1:length(list_of_dfs )
-        length_df     <- do.call(rbind, (lapply( list_of_dfs, function(x) length(x))))
-        max_no        <- max(length_df[,1])
-        
-        if ( length(names(length_df)) == 0 ) {
-          
-          names_list<- ""
-        }
-        
-        if ( length(names(length_df)) != 0 ) {
-          
-          max_df        <- length_df[max(length_df),]
-          name_df       <- names(length_df[length_df== max_no][1])
-          names_list    <- names(list_of_dfs[ name_df][[1]])
-          
-        }
-        
-      }
-      
-      
-      
-      if ( length(names(list_of_dfs)) != 0 ) {
-        length_df     <- do.call(rbind, (lapply( list_of_dfs, function(x) length(x))))
-        max_no        <- max(length_df[,1])
-        
-        if ( length(names(length_df)) == 0 ) {
-          
-          names_list<- ""
-        }
-        
-        if ( length(names(length_df)) != 0 ) {
-          
-          max_df        <- length_df[max(length_df),]
-          name_df       <- names(length_df[length_df== max_no][1])
-          names_list    <- names(list_of_dfs[ name_df][[1]])
-          
-        }
-        
-      }
-      
-      
-      df_dfs <- list()
-      for (i in 1:max_no ) {
-        
-        df_dfs[[i]]            <- do.call(rbind, lapply(1:length(list_of_dfs), function(x) list_of_dfs[[x]][i]))
-        
-      }
-      
-      
-      df_cbind               <- do.call( cbind, df_dfs )
-      
-      if ( length(rownames( df_cbind ) ) !=   length(rownames (length_df) ))  {
-        
-        rownames (length_df) <- rownames (length_df)
-      }
-      
-      if ( length(rownames( df_cbind ) ) ==  length(rownames (length_df) ))  {
-        
-        rownames( df_cbind )   <- rownames (length_df)
-      }
-      
-      
-      if( names_list != "") {
-        
-        colnames( df_cbind )   <- names_list
-        
-      }
-      
-      
-      df_cbind
-      
-    }
-    
-    
-    
-    #### Rbinding the datasets 
-    var_names[[d]]   <- trimws(substr( test_nm[[1]], st_pos, fn_pos))
-    
-    rbind_cross[[d]] <- rbind_null_df_lists( tables_ln )
-    rbind_chsq[[d]]  <- rbind_null_df_lists( chsq_lns )
-  }
+  # For multiple crosstab csv files from PSPP in same folder to process.
+  # Only when horizontal categories are same
+  crosstbs <- csv_files
+  crosstbs$id <- 1:(dim( crosstbs )[1])
   
   
-  ####  Adding names to the dataset
-  names( rbind_cross )  <- var_names
-  names( rbind_chsq )   <- var_names
+  # What row results to include in final crosstab results
+  if ('Count'     %in% tbl_rows )   tbl_Count     = 'Count'         else if ('Count'     %!in%  tbl_rows)     tbl_Count     = 'NAN' 
+  if ('Row'       %in% tbl_rows )   tbl_Row       = 'Row %'         else if ('Row'       %!in%  tbl_rows)     tbl_Row       = 'NAN' 
+  if ('Col'       %in% tbl_rows )   tbl_Col       = 'Col %'         else if ('Col'       %!in%  tbl_rows)     tbl_Col       = 'NAN' 
+  if ('rTotal'    %in% tbl_rows )   tbl_rTotal    = 'Total %'       else if ('rTotal'    %!in%  tbl_rows)     tbl_rTotal    = 'NAN' 
+  # Defining how many rows the rowsumtotal is using
+  if (rsumTotal    !=         0 )   tbl_rsumTotal = rsumTotal       else if (rsumTotal    ==           0)     tbl_rsumTotal = 0 # How many positions to let grand total appear?
+  
+  # Dont include row results that user excluded
+  include_tbl <- c(tbl_Count, tbl_Row, tbl_Col, tbl_rTotal)
+  exclude_nan <- include_tbl[include_tbl %!in% "NAN" ] 
   
   
-  ### Finding the dataset with the whole "label" categories
-  lgth_list     <-  length( rbind_cross )
-  rlist_labels  <-  lapply( 1:lgth_list, function(x)  rbind_cross[[x]][1,][ rbind_cross[[x]][1,] != ""] )
-  lbls_unique   <-  unique( unlist( rlist_labels ) )
-  rlist_length  <-  unlist( lapply( 1:lgth_list, function(x)  length( rbind_cross[[x]][1,][ rbind_cross[[x]][1,] != ""]) ) )
-  max_lbls      <-  length( lbls_unique )
-  seq_max       <-  1:max_lbls
+  # Getting the last column of numerical tables
+  min_dim_var_Tot <- crosstbs[ ,min(dim(crosstbs))-1] 
+  # tbl_st tbl_fn - start/fin of numerical tables
+  tbl_st          <- crosstbs[crosstbs$id[ grepl(x=min_dim_var_Tot, "Total") ] + 2, ] 
+  tbl_fn          <- crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], "Total") ], ] 
+  # number of tables
+  no_tbls         <- length(tbl_st$id)
+  # Which rows to include? 
+  dims_no_st_fn   <- sapply(1:no_tbls, function(i) seq( tbl_st$id[i],  tbl_fn$id[i] -1 + tbl_rsumTotal)) 
+  # The numerical tables as list
+  cross_tbls      <- sapply(1:no_tbls, function(i) list(  rbind(crosstbs[ dims_no_st_fn[[i]], ], rep("", min(dim(crosstbs)) ) ) ) )
+  # The numerical tables as binded
+  crss_tbls_binded <- do.call("rbind", cross_tbls)
+  # adding colanmes
+  colnames(crss_tbls_binded) <- crosstbs[ dims_no_st_fn[[1]][1] -1, ]
   
-  # rlist_max_lg  <-  which(max( rlist_length ) == rlist_length )[1]
-  # rlist_max_labels <-  rlist_labels[[ rlist_max_lg ]]
-  # rl_max  <-  rlist_max_labels
+  # Naming empty cols
+  empty_colnms   <- colnames(crss_tbls_binded) %in% "" 
+  nm_empty_cols  <- colnames(crosstbs)[empty_colnms]
+  nm_empty_cols  <- c("Var", "Levels", "Counts", "Totals")
+  colnames(crss_tbls_binded)[empty_colnms] <- nm_empty_cols
+  # Naming last "ID" column
+  colnames(crss_tbls_binded)[ min(dim(crosstbs)) ] <- "id"
   
-  # Aligning the variables / columns into the right position 
-  # Finding the incorrect position ( pos_st )
-  # Finding the correct position( pos_orig )
-  pos_orig       <- rbind_null_df_lists( lapply(1:lgth_list, function(a) na.omit(match(as.matrix(rbind_cross[[a]][1,] ), lbls_unique  ))) )
-  pos_st         <- rbind_null_df_lists( lapply(1:lgth_list, function(b) unlist(lapply(1:length(lbls_unique), function(y) match(lbls_unique[[y]], rlist_labels[[b]]) )) ))
+  # final crosstabs data
+  final_crosstbl <- crss_tbls_binded[crss_tbls_binded$Counts %in% exclude_nan, ]
   
+  #####################################################################################
+  #####################################################################################
   
-  # Aligning columns / variables
-  rdt_alignd <- list()
-  for (i in 1:lgth_list ) {
-    
-    rc         <- rbind_cross
-    rdt_alignd[[i]] <- cbind(rbind_cross[[i]]$id, rc[[i]][ na.omit(pos_st[i,]) ])
-    
-    
-  }
+  # Extracting pearson r and sig. of it and nvalid cases
+  st_pchisq          <-crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], "Pearson Chi-Square") ], ][-1][,1:3]
+  st_nvalid          <-crosstbs[crosstbs$id[ grepl(x=crosstbs[[1]], "Linear-by-Linear Association") ] + 1, ][2]
+  colnames(st_pchisq) <- c("chisq", "df", "p")
+  colnames(st_nvalid) <- c("n.valid")
   
+  # Extracing crosstab stats
+  st_cramer          <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "Cramer") ],      ][[3]] )
+  st_phi             <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "Phi") ],         ][[3]] )
+  st_contingency     <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "Contingency") ], ][[3]] )
+  st_taub            <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "tau-b")       ], ][[3]] )
+  st_tauc            <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "tau-c") ],       ][[3]] )
+  st_gamma           <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "Gamma") ],       ][[3]] )
+  st_spearman        <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "Spearman") ],    ][[3]] )
+  st_pearson         <-as.data.frame( crosstbs[crosstbs$id[ grepl(x=crosstbs[[2]], "Pearson") ],     ][[3]] )
   
-  horizontally_aligned   <- do.call(cbind, rdt_alignd )
-  vertically_aligned     <- rdt_alignd
+  # colnaming crosstab stats
+  colnames(st_cramer)           <- "Cramers v"
+  colnames(st_phi)              <- "Phi"
+  colnames(st_contingency)      <- "Contingency Coeff."
+  colnames(st_taub)             <- "Kendalls tau-b"
+  colnames(st_tauc)             <- "Kendalls tau-c"
+  colnames(st_gamma)            <- "Gamma"
+  colnames(st_spearman)         <- "Spearman corr"
+  colnames(st_pearson)          <- "Pearsons r"
   
+  # If else for crosstab stats
+  chq_pchisq = st_pchisq 
+  chq_nvalid = st_nvalid
+  if ('Cramersv'    %in% chqstats )   chq_cramersv  = st_cramer       else if ('Cramersv'     %!in%  chqstats)     chq_cramersv    = 'NAN' 
+  if ('Phi'         %in% chqstats )   chq_phi       = st_phi          else if ('Phi'          %!in%  chqstats)     chq_phi         = 'NAN' 
+  if ('Contingency' %in% chqstats )   chq_conting   = st_contingency  else if ('Contingency'  %!in%  chqstats)     chq_conting     = 'NAN' 
+  if ('taub'        %in% chqstats )   chq_taub      = st_taub         else if ('taub'         %!in%  chqstats)     chq_taub        = 'NAN' 
+  if ('tauc'        %in% chqstats )   chq_tauc      = st_tauc         else if ('tauc'         %!in%  chqstats)     chq_tauc        = 'NAN' 
+  if ('Gamma'       %in% chqstats )   chq_gamma     = st_gamma        else if ('Gamma'        %!in%  chqstats)     chq_gamma       = 'NAN' 
+  if ('Spearman'    %in% chqstats )   chq_spearman  = st_spearman     else if ('Spearman'     %!in%  chqstats)     chq_spearman    = 'NAN' 
+  if ('Pearson'     %in% chqstats )   chq_pearson   = st_pearson      else if ('Pearson'      %!in%  chqstats)     chq_pearson     = 'NAN' 
   
-  # Creating an empty file for vertical-type format table
-  file.create( paste0(pth, "rbind_cross.csv") )
+  # Binding all non nan crosstabs stats
+  chqstats_all      <- c(chq_cramersv, chq_phi,  chq_conting, chq_taub, chq_tauc, chq_gamma, chq_spearman, chq_pearson) 
+  chqstats_nan      <- chqstats_all  %!in% "NAN"
+  chqstats_df       <- c(chq_cramersv, chq_phi,  chq_conting, chq_taub, chq_tauc, chq_gamma, chq_spearman, chq_pearson)
+  chqstats_df_final <- cbind( chq_pchisq, chq_nvalid, chqstats_all[ chqstats_nan ] )
   
-  # Appending the results for vertical-type format table
-  lapply( 1:lgth_list, function(x)write.table( vertically_aligned[[x]],  append = TRUE, paste0(pth, "rbind_cross.csv") ) )
+
+  #####################################################################################
+  #####################################################################################
   
   # Creating an empty file for horizontal-type format table
-  file.create( paste0(pth, "cbind_cross.csv") )
+  file.create( paste0(path_csv, "chqstats.csv") )
   
-  # Appending the results for horizontal-type format table
-  write.table( horizontally_aligned,  append = TRUE, paste0(pth, "cbind_cross.csv") )
-  
+  # Writing results to the path given
+  write.table( final_crosstbl,     append = TRUE, paste0(path_csv, "chqstats.csv") )
+  write.table( chqstats_df_final,  append = TRUE, paste0(path_csv, "chqstats.csv") )
   
 }
-
 
 
 
@@ -245,34 +174,16 @@ crosstabs_same_horizontal_categories <- function( path_of_csvs ) {
 ####           Running  The Example                  #####
 ##########################################################
 
-
-
 #### Downloading example crosstab csv files from github of "estatistics/psychology"
 #### !!! please uncomment the following lines in order to run the example !!!
-
-
+#
 # require(RCurl)
-# crosstabs_01 <-read.csv(text=getURL("https://raw.githubusercontent.com/estatistics/psychology/master/crosstabs_01.csv" ), 
-#                         skip=0, header=F,  sep = "," )
-# crosstabs_02 <-read.csv(text=getURL("https://raw.githubusercontent.com/estatistics/psychology/master/crosstabs_02.csv"), 
-#                         skip=0, header=F,  sep = ","  )
-# 
-# 
-# 
-# Declare the path to save example csv files
-# For window users, please change it accordingly...
-# path_of_csvs <- "/tmp/"
-# 
-# 
-# # writing the files on the specified path
-# write.table( crosstabs_01,  append = F, paste0(path_of_csvs, "cross_01.csv"), sep = "," )
-# write.table( crosstabs_02,  append = F, paste0(path_of_csvs, "cross_02.csv"), sep = "," )
-
-
-
-##### Please uncomment the below line/s in order to run the example 
-
-# crosstabs_same_horizontal_categories ( path_of_csvs )
-
-  
+#
+# crosstabs_01 <-read.csv(text=getURL("https://raw.githubusercontent.com/estatistics/psychology/master/crosstabs_samedata_horizontal_example.csv" ), 
+#                         skip=0, header=F,  sep = "," )# 
+# crosstabs_same_horizontal_labels(
+#  path_csv="", file_name="crosstabs_01", csv_opts=FALSE,
+#  tbl_rows=c('Count','Row', "rTotal"), rsumTotal=0, 
+#  chqstats=c('Cramersv', 'Phi',  'Contingency',  'Spearman', 'Pearson')
+# )
   
